21b92fb9d3394b58cf8b09694fc1be9c85b28921
David Sánchez <davidslt+git@pm.me>
2023-12-02 12:48:03 +0000

    build: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Advent of Code 2023 in Haskell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              actionlint.enable = true;
+              alejandra.enable = true;
+              cabal-fmt.enable = true;
+              cabal2nix.enable = true;
+              convco.enable = true;
+              ormolu.enable = true;
+              hlint.enable = true;
+              hpack.enable = true;
+              # hunspell.enable = true;
+            };
+            settings = {
+              alejandra.exclude = ["default.nix"];
+            };
+          };
+        };
+        devShells = {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = with pkgs.haskellPackages; [
+              ghc
+              cabal-install
+              haskell-language-server
+            ];
+          };
+        };
+        packages = {
+          default = pkgs.haskellPackages.callPackage ./default.nix {};
+        };
+      }
+    );
+}
