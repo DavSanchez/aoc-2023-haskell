e781b0f9587a24b7d0329826fce2cfaf63970eb6
David Sánchez <davidslt+git@pm.me>
2023-12-08 23:07:02 +0000

    chore: prepare day 09

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Day05 (day05)
 import Day06 (day06)
 import Day07 (day07)
 import Day08 (day08)
+import Day09 (day09)
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -22,4 +23,5 @@ main = do
     "6" : _ -> day06
     "7" : _ -> day07
     "8" : _ -> day08
+    "9" : _ -> day09
     _ -> error "None or invalid day number provided."
